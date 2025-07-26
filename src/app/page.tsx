504a9985db372951e5b802a7632e51c0b0ebc047
Diego Vázquez Campos (Kaerit) <kaerit@hotmail.com>
2025-07-26 22:34:30 +0000

    vale pero ahora en la previsulaicación de aqui me sale 404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,5 @@
 import { redirect } from 'next/navigation'
 
 export default function RootPage() {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-  redirect(`${basePath}/es`)
+  redirect('/es')
 }
