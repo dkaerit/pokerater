64bf9f65b370a88582c062e9cbbd5cb95a660e72
Diego Vázquez Campos (Kaerit) <kaerit@hotmail.com>
2025-07-26 18:26:28 +0000

    puedes hacer que los colores que asignaste para la línea del grafico sea

--- src/components/scoreboard.tsx
+++ src/components/scoreboard.tsx
@@ -45,12 +45,12 @@ export function Scoreboard({ scores }: ScoreboardProps) {
                   <stop offset={1} stopColor="#380815" stopOpacity={0.8}/>
                 </linearGradient>
                 <linearGradient id="splitColorStroke" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset={0.5 / 6} stopColor="#033136" />
-                  <stop offset={2.5 / 6} stopColor="#033136" />
-                  <stop offset={2.5 / 6} stopColor="#3a2f0f" />
-                  <stop offset={4.5 / 6} stopColor="#3a2f0f" />
-                  <stop offset={4.5 / 6} stopColor="#380815" />
-                  <stop offset={1} stopColor="#380815" />
+                  <stop offset={0.5 / 6} stopColor="#054b53" />
+                  <stop offset={2.5 / 6} stopColor="#054b53" />
+                  <stop offset={2.5 / 6} stopColor="#5c4a18" />
+                  <stop offset={4.5 / 6} stopColor="#5c4a18" />
+                  <stop offset={4.5 / 6} stopColor="#570d22" />
+                  <stop offset={1} stopColor="#570d22" />
                 </linearGradient>
               </defs>
               <CartesianGrid vertical={false} strokeDasharray="3 3" />
