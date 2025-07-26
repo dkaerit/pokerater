ca39e54735767ac012b5c777d733498e1be92728
Diego Vázquez Campos (Kaerit) <kaerit@hotmail.com>
2025-07-26 18:23:47 +0000

    no tan pastel, mas bien cercano a esto  #380815, #380815, #380815

--- src/components/scoreboard.tsx
+++ src/components/scoreboard.tsx
@@ -45,12 +45,12 @@ export function Scoreboard({ scores }: ScoreboardProps) {
                   <stop offset={1} stopColor="#380815" stopOpacity={0.8}/>
                 </linearGradient>
                 <linearGradient id="splitColorStroke" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset={0.5 / 6} stopColor="#a7d8de" />
-                  <stop offset={2.5 / 6} stopColor="#a7d8de" />
-                  <stop offset={2.5 / 6} stopColor="#f3e5ab" />
-                  <stop offset={4.5 / 6} stopColor="#f3e5ab" />
-                  <stop offset={4.5 / 6} stopColor="#f4cccc" />
-                  <stop offset={1} stopColor="#f4cccc" />
+                  <stop offset={0.5 / 6} stopColor="#033136" />
+                  <stop offset={2.5 / 6} stopColor="#033136" />
+                  <stop offset={2.5 / 6} stopColor="#3a2f0f" />
+                  <stop offset={4.5 / 6} stopColor="#3a2f0f" />
+                  <stop offset={4.5 / 6} stopColor="#380815" />
+                  <stop offset={1} stopColor="#380815" />
                 </linearGradient>
               </defs>
               <CartesianGrid vertical={false} strokeDasharray="3 3" />
