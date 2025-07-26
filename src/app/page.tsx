6346d2ac2b2d527a218edb5605ea022a86bd73ff
kaerit <kaerit@hotmail.com>
2025-07-26 21:13:10 +0000

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import { redirect } from 'next/navigation'
-
-export default function RootPage() {
-  redirect('/es')
-}
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="refresh" content="0;url=/es">
+</head>
+<body>
+  <p>Redirecting to <a href="/es">/es</a></p>
+</body>
+</html>
